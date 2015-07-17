acbff73472d8e0a1e2fb5ee88e3f573279199e13
Matthew Medal <mmedal@ebay.com>
2015-07-16 17:09:46 -0700

    magicslack for heroku

---
+++ Magic_the_Slackening/settings.py
@@ -0,0 +1,86 @@
+"""
+Django settings for Magic_the_Slackening project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'e$7q+y%yz$v%c(8kojtw7^mo&p%rcm)2%tz*yq!y(&g&q%(4@$'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'Magic_the_Slackening.urls'
+
+WSGI_APPLICATION = 'Magic_the_Slackening.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
---
+++ Magic_the_Slackening/views.py
@@ -0,0 +1,31 @@
+from os import environ
+import urllib
+
+from rest_framework.exceptions import ParseError, PermissionDenied
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+gatherer_uri = 'http://gatherer.wizards.com/Handlers/Image.ashx?type=card'
+
+
+class SlackMagicCardView(APIView):
+    """
+    Slack webhook interface for returning details of magic card.
+    """
+    def post(self, request):
+        # if 'token' not in request.data:
+        #     raise PermissionDenied
+        # if request.data['token'] != environ['SLACK_TOKEN']:
+        #     raise PermissionDenied
+
+        if 'text' not in request.data:
+            raise ParseError
+
+        card_name = request.data['text'][9:].strip(' ')
+        card_img_uri = '{}&name={}'.format(gatherer_uri, urllib.quote_plus(card_name))
+
+        return Response({
+            'text': card_img_uri
+        })
+
