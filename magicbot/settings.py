fc22417a49224aeb8c3ed8915779a1fd30be54c5
Matthew Medal <mmedal@ebay.com>
2016-03-01 15:59:10 -0800

    favicon and rename

--- magicbot/settings.py
+++ magicbot/settings.py
@@ -55,6 +55,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 TEMPLATES = [
     {
--- magicbot/urls.py
+++ magicbot/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
+from django.views.generic.base import RedirectView
 
 from views import MagicCardView
 
 
 urlpatterns = [
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/img/favicon.ico')),
     url(r'^magic-cards$', MagicCardView.as_view())
 ]
